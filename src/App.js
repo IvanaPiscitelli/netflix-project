973bec6528762fc41326927cdaa4c4ac791b5552
ivana piscitelli <85pkRWi7LF>
2023-03-09 11:04:32 +0100

    finish setup movie component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { fetchMovies } from "./utils/axios";
 import { BASE_IMAGE, requests, responsive } from "./utils/const";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Movie from "./components/Movies/Movies/Movie";
-import style from "./index.css";
+import Movie from "./components/Movies/Movie";
 
 function App() {
   const [trendingMovies, setTrendingMovies] = useState([]);
@@ -24,17 +23,17 @@ function App() {
 
   return (
     <main>
-      <Carousel
-        responsive={responsive}
-        infinite
-        customTransition="transform 330ms ease-out 50ms"
-        customLeftArrow={style.leftArrow}
-        customRightArrow={style.rightArrow}
-      >
+      <Carousel responsive={responsive} infinite customTransition="transform 330ms ease-out 50ms">
         {trendingMovies.map((trendingMovie) => {
           return <Movie key={trendingMovie.id} poster={`${BASE_IMAGE}${trendingMovie.poster_path}`} />;
         })}
       </Carousel>
+      <Carousel responsive={responsive} infinite customTransition="transform 330ms ease-out 50ms">
+        {romanceMovies.map((romanceMovie) => {
+          return <Movie key={romanceMovie.id} poster={`${BASE_IMAGE}${romanceMovie.poster_path}`} />;
+        })}
+      </Carousel>
+
       <Carousel responsive={responsive} infinite customTransition="transform 330ms ease-out 50ms">
         {romanceMovies.map((romanceMovie) => {
           return <Movie key={romanceMovie.id} poster={`${BASE_IMAGE}${romanceMovie.poster_path}`} />;
